b35b6da8bf734138fe07c14a1a5a4af25df676d5
Yuriy Zhayvoronok <yura@zhayvoronok.com>
2019-10-27 21:06:17 +0300

    WIP: train function

--- network.py
+++ network.py
@@ -15,7 +15,7 @@ class Network:
     
     self.wih = numpy.random.normal(
       0.0, pow(self.hnodes, -0.5), (self.hnodes, self.inodes))
-    self.w_hidden_output = numpy.random.normal(
+    self.who = numpy.random.normal(
       0.0, pow(onodes, -0.5), (self.onodes, self.hnodes))
   
   def query(self, input_list):
@@ -24,10 +24,29 @@ class Network:
     hidden_inputs = numpy.dot(self.wih, inputs)
     hidden_outputs = self.afunc(hidden_inputs)
 
-    output_inputs = numpy.dot(self.who, hidden_outputs)
-    output_outputs = self.afunc(output_inputs)
+    final_inputs = numpy.dot(self.who, hidden_outputs)
+    final_outputs = self.afunc(final_inputs)
 
-    return output_outputs
+    return final_outputs
 
-  def train(self):
-    pass
+  def train(self, input_list, target_list):
+    inputs = numpy.array(input_list, ndmin=2).T
+    targets = numpy.array(target_list, ndmin=2).T
+
+    hidden_inputs = numpy.dot(self.wih, inputs)
+    hidden_outputs = self.afunc(hidden_inputs)
+
+    final_inputs = numpy.dot(self.who, hidden_outputs)
+    final_outputs = self.afunc(final_inputs)
+
+    output_errors = targets - final_outputs
+    hidden_errors = numpy.dot(self.who.T, output_errors)
+
+    self.who += self.lgrade * numpy.dot(
+      output_errors * final_outputs * (1 - final_outputs), 
+      hidden_outputs.T)
+
+    self.wih += self.lgrade * numpy.dot(
+      hidden_errors * hidden_outputs * (1 - hidden_outputs),
+      inputs.T)
+
